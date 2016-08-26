986d86fc9aa9901d65ef457acd9675e9f3fc1834
Unibix.ru <admin@usercms.ru>
2016-08-26 15:33:32 +0200

    Modified and optimized fancyBox addon

--- www/user_cms/modules/addons/fancybox/controller_addon_core_fancybox.php
+++ www/user_cms/modules/addons/fancybox/controller_addon_core_fancybox.php
@@ -11,9 +11,7 @@ class controller_addon_core_fancybox extends addon  {
 		
 		$this->page['head'] .= $this->add_css_file(SITE_URL . '/user_cms/modules/addons/fancybox/source/jquery.fancybox.css?v=2.1.5') ;
 		
-		$this->page['head'] .= $this->add_css_file(SITE_URL . '/user_cms/modules/addons/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7');		
-		$this->page['head'] .= $this->add_js_file(SITE_URL . '/user_cms/modules/addons/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7');
-		$this->page['head'] .= $this->add_js_file(SITE_URL . '/user_cms/modules/addons/fancybox/source/helpers/jquery.fancybox-settings.js?v=1.0.7');
+		$this->page['head'] .= $this->add_js("\t\t" . '$(document).ready(function(){$(".fancybox").fancybox({openEffect:"elastic",openSpeed:450,closeEffect:"elastic",closeSpeed:250,nextClick:!0,helpers:{title:{type:"over"}}})});' ) ;
 
 		$this->page['head']  .= "\t\t<!-- END fancybox -->\n";
 		return $this->page;
