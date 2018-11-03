17be6b1c4e2bce1534725073f14f71ed28763bf8
Зарзанд Мхитарян <zmkhitaryan88@gmail.com>
2018-11-03 20:21:23 +0200

    fixed jodit console error on admin panel

--- www/user_cms/modules/addons/jodit/controller_addon_core_jodit.php
+++ www/user_cms/modules/addons/jodit/controller_addon_core_jodit.php
@@ -5,9 +5,9 @@ class controller_addon_core_jodit extends addon {
     $this->page['head'] .= $this->add_css_file(SITE_URL . '/user_cms/modules/addons/jodit/jodit.min.css'). "\n\t";
     $this->page['head'] .= $this->add_js_file(SITE_URL . '/user_cms/modules/addons/jodit/jodit.min.js'). "\n\t";
     $this->page['head'] .= $this->add_js(
-
-    "$(document).ready(function() { 
-		new Jodit('.wysiwyg', {
+    "$(document).ready(function() {
+		if($('textarea').is('.wysiwyg')){
+			new Jodit('.wysiwyg', {
 		        uploader: {
 		            url: '" . SITE_URL . "/user_cms/modules/addons/jodit/connector/index.php?action=fileUpload'
 		        },
@@ -18,19 +18,14 @@ class controller_addon_core_jodit extends addon {
 		        },
 		        minHeight: 350
 		    });
-
+		}
 	 });"
 
     ) 
 
     . "\n\t";
-    
-
-
+   
     $this->page['head'] .= '<!-- END jodit editor -->' . "\n\t";
-
-
-		
 		return $this->page;
 	}
 
