caa3fc0ed074354059a5289d8ee2cf3ff9733216
unibix <info@unibix.ru>
2019-03-29 14:42:25 +0200

    Added html mode to codemirror

--- www/user_cms/modules/addons/codemirror/controller_addon_core_codemirror.php
+++ www/user_cms/modules/addons/codemirror/controller_addon_core_codemirror.php
@@ -9,7 +9,8 @@ class controller_addon_core_codemirror extends addon  {
 		
 		$this->page['head'] .= $this->add_css_file(SITE_URL . '/user_cms/modules/addons/codemirror/source/codemirror.css?v=5.40.2') ;
 		$this->page['head'] .= $this->add_js_file (SITE_URL . '/user_cms/modules/addons/codemirror/source/codemirror.js?v=5.40.2') ;
-
+		$this->page['head'] .= $this->add_css_file('https://codemirror.net/theme/blackboard.css') ;
+		$this->page['head'] .= $this->add_js_file ('https://codemirror.net/mode/xml/xml.js') ;
 		$this->page['head'] .= $this->add_js('
 
 			$(document).ready(function() {
