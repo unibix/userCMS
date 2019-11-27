60c394fd6548508f24c1a2eb3573eaf3dfb67d4b
Vladimir <vladimir-orlov7@mail.ru>
2019-11-27 19:37:53 +0300

    Добавлена text/plain версия письма

--- www/user_cms/helpers/helper_mail.php
+++ www/user_cms/helpers/helper_mail.php
@@ -27,6 +27,8 @@ class helper_mail {
         $mb_internal_encoding = mb_internal_encoding();
         mb_internal_encoding('UTF-8');
 
+        $plain_message = strip_tags($message);
+
         $headers = "Date: ".date("r")."\r\n";
         $headers.= "From: =?UTF-8?B?".base64_encode($this->from_name)."?= <".$this->from_email.">\r\n";
         $headers.= "Reply-To: <".$this->from_email.">\r\n";
@@ -34,19 +36,22 @@ class helper_mail {
         $headers.= "MIME-Version: 1.0\r\n";
 
         $subject = "=?UTF-8?B?".base64_encode($this->subject)."?=";
-        if (strpos($message, '</')) $msgType = "text/html"; else $msgType = "text/plain";
         
         $body = '';
-        
+        $boundary = md5(time());
+        $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
         if ($this->attach) {
-            $boundary = md5(time());
-            $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
 
             $body  = "\r\n--$boundary\r\n"; 
-            $body .= "Content-Type: $msgType; charset=UTF-8\r\n";
+            $body .= "Content-Type: text/html; charset=UTF-8\r\n";
             $body .= "Content-Transfer-Encoding: 8bit\r\n";
             $body .= "\r\n";
             $body .= '<html>' . $message . '</html>';
+            $body .= "\r\n--$boundary\r\n"; 
+            $body .= "Content-Type: text/plain; charset=UTF-8\r\n";
+            $body .= "Content-Transfer-Encoding: 8bit\r\n";
+            $body .= "\r\n";
+            $body .= $plain_message;
 
             foreach ($this->attach as $path) {
                 $filename = mb_substr($path, mb_strrpos($path, '/'));
@@ -57,15 +62,20 @@ class helper_mail {
                 $body .= "\r\n";
                 $body .= chunk_split(base64_encode(file_get_contents($path)));
             }
-            
-            $body .= "\r\n--$boundary--\r\n";
         
         } else {
-            $headers .= "Content-Type: $msgType; charset=UTF-8\r\n";
-            $headers .= "Content-Transfer-Encoding: 8bit\r\n";
-            $headers .= "\r\n";
+            $body .= "\r\n--$boundary\r\n"; 
+            $body .= "Content-Type: text/html; charset=UTF-8\r\n";
+            $body .= "Content-Transfer-Encoding: 8bit\r\n";
+            $body .= "\r\n";
             $body .= '<html>' . $message . '</html>';
+            $body .= "\r\n--$boundary\r\n"; 
+            $body .= "Content-Type: text/plain; charset=UTF-8\r\n";
+            $body .= "Content-Transfer-Encoding: 8bit\r\n";
+            $body .= "\r\n";
+            $body .= $plain_message;
         }
+        $body .= "\r\n--$boundary--\r\n";
         mb_internal_encoding($mb_internal_encoding);
         return mail($this->mail_target, $subject, $body, $headers);
     }
